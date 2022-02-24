7052c3a3d137569632cd39d463659a25ac20a601
Your Name <shibu.a5859@gmail.com>
2022-02-24 20:01:52 +0530

    after error fixing

--- config/connection.js
+++ config/connection.js
@@ -4,15 +4,7 @@ const MongoClient = mongodb.MongoClient
 const state = {
     db: null
 }
-<<<<<<< HEAD
-module.exports.connection = function (done) {
-    //  const url='mongodb://127.0.0.1:27017'    
-    const dbname = 'eCommerce'
-    MongoClient.connect(process.env.MONGO_URL, { useNewUrlParser: true }, (err, data) => {
-        if (err) return done(err)
-        state.db = data.db(dbname)
-        done()
-=======
+
 module.exports.connection=function(done){
    const url='mongodb+srv://shibu:123@cluster0.jxuep.mongodb.net/eCommerce?retryWrites=true&w=majority'
     const dbname='eCommerce'
@@ -20,8 +12,6 @@ MongoClient.connect(url,{useNewUrlParser:true},(err,data)=>{
   if(err)return done (err)
  state.db=data.db(dbname)
    done()
->>>>>>> 2cd0c5795dd03f0e963cc565eb5f2f8c21deb4d5
-
     })
 }
 module.exports.get = () => {
