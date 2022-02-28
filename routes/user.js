430cdaef0d3d1116b4725a3594ce9275056323de
Shibuam <91356491+Shibuam@users.noreply.github.com>
2022-02-28 19:51:24 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -506,8 +506,8 @@ router.get('/pay', (req, res) => {
       "payment_method": "paypal"
     },
     "redirect_urls": {
-      "return_url": "http://localhost:3000/success",
-      "cancel_url": "http://localhost:3000/cancel"
+      "return_url": "https://shopgrids.online/success",
+      "cancel_url": "https://shopgrids.online/cancel"
     },
     "transactions": [{
       "item_list": {
